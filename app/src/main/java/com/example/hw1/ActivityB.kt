b075e329043b89496c3553355f737dfdc75dab6e
fxn <fxn@fxn.fxn>
2024-10-19 23:00:02 +0300

    part 1

--- app/src/main/java/com/example/hw1/ActivityB.kt
+++ app/src/main/java/com/example/hw1/ActivityB.kt
@@ -1,36 +1,29 @@
 package com.example.hw1
 
 import android.content.Intent
-import android.content.res.Configuration
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ActivityA : AppCompatActivity() {
+class ActivityB : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_a)
+        setContentView(R.layout.activity_b)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btnOpenActivityB = findViewById<Button>(R.id.btnOpenActivityB)
-        val btnOpenFragmentB = findViewById<Button>(R.id.btnOpenFragmentB)
 
-        btnOpenActivityB.setOnClickListener {
-            val intent = Intent(this, ActivityB::class.java)
-                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        val btnOpenActivityC = findViewById<Button>(R.id.btnOpenActivityC)
+
+        btnOpenActivityC.setOnClickListener {
+            val intent = Intent(this, ActivityC::class.java)
             startActivity(intent)
-        }
-        btnOpenFragmentB.setOnClickListener {
 
         }
     }
--- app/src/main/java/com/example/hw1/ActivityC.kt
+++ app/src/main/java/com/example/hw1/ActivityC.kt
@@ -1,37 +1,29 @@
 package com.example.hw1
 
 import android.content.Intent
-import android.content.res.Configuration
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ActivityA : AppCompatActivity() {
+class ActivityC : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_a)
+        setContentView(R.layout.activity_c)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btnOpenActivityB = findViewById<Button>(R.id.btnOpenActivityB)
-        val btnOpenFragmentB = findViewById<Button>(R.id.btnOpenFragmentB)
 
-        btnOpenActivityB.setOnClickListener {
-            val intent = Intent(this, ActivityB::class.java)
-                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        val btnOpenActivityA = findViewById<Button>(R.id.btnOpenActivityA)
+
+        btnOpenActivityA.setOnClickListener {
+            val intent = Intent(this, ActivityA::class.java)
             startActivity(intent)
-        }
-        btnOpenFragmentB.setOnClickListener {
-
         }
     }
 }
