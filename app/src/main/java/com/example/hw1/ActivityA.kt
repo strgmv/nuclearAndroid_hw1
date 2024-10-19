688703cd5be24253a638cd5bc4f8d0ba9b1d1ba0
fxn <fxn@fxn.fxn>
2024-10-19 22:55:02 +0300

    part 0

---
+++ app/src/main/java/com/example/hw1/ActivityA.kt
@@ -0,0 +1,35 @@
+package com.example.hw1
+
+import android.content.Intent
+import android.content.res.Configuration
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.FrameLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ActivityA : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_a)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnOpenActivityB = findViewById<Button>(R.id.btnOpenActivityB)
+        val btnOpenFragmentB = findViewById<Button>(R.id.btnOpenFragmentB)
+
+        btnOpenActivityB.setOnClickListener {
+
+        }
+        btnOpenFragmentB.setOnClickListener {
+
+        }
+    }
+}
